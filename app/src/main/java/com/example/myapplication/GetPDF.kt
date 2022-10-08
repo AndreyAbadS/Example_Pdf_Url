3b9ed0edc986ae3480d589cf8dbb88e3ead3b905
Bside - Jorge Andrey Abad Salinas <bside.jorge@coppel.com>
2022-10-07 19:20:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/GetPDF.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication
+
+import android.os.AsyncTask
+import com.github.barteksc.pdfviewer.PDFView
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+class GetPDF(pdfView: PDFView): AsyncTask<String,Void,InputStream>() {
+    val mypdfView: PDFView = pdfView
+    override fun doInBackground(vararg params: String?): InputStream? {
+        var inputStream: InputStream? = null
+        try {
+            // on below line we are creating an url
+            // for our url which we are passing as a string.
+            val url = URL(params.get(0))
+
+            // on below line we are creating our http url connection.
+            val urlConnection: HttpURLConnection = url.openConnection() as HttpsURLConnection
+
+            // on below line we are checking if the response
+            // is successful with the help of response code
+            // 200 response code means response is successful
+            if (urlConnection.responseCode == 200) {
+                // on below line we are initializing our input stream
+                // if the response is successful.
+                inputStream = BufferedInputStream(urlConnection.inputStream)
+            }
+        }
+        // on below line we are adding catch block to handle exception
+        catch (e: Exception) {
+            // on below line we are simply printing
+            // our exception and returning null
+            e.printStackTrace()
+            return null;
+        }
+        // on below line we are returning input stream.
+        return inputStream;
+    }
+    override fun onPostExecute(result: InputStream?) {
+        // on below line we are loading url within our
+        // pdf view on below line using input stream.
+        mypdfView.fromStream(result).load()
+    }
+
+}
