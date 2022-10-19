458ea273ce70fdc379aa802cec1741016c08418c
Bside.jorge <andreyabadsalinas2@gmail.com>
2022-10-19 17:13:45 -0500

    commit 3

--- app/src/main/java/com/example/myapplication/GetPDF.kt
+++ app/src/main/java/com/example/myapplication/GetPDF.kt
@@ -13,35 +13,19 @@ class GetPDF(pdfView: PDFView): AsyncTask<String,Void,InputStream>() {
     override fun doInBackground(vararg params: String?): InputStream? {
         var inputStream: InputStream? = null
         try {
-            // on below line we are creating an url
-            // for our url which we are passing as a string.
             val url = URL(params.get(0))
-
-            // on below line we are creating our http url connection.
             val urlConnection: HttpURLConnection = url.openConnection() as HttpsURLConnection
-
-            // on below line we are checking if the response
-            // is successful with the help of response code
-            // 200 response code means response is successful
             if (urlConnection.responseCode == 200) {
-                // on below line we are initializing our input stream
-                // if the response is successful.
                 inputStream = BufferedInputStream(urlConnection.inputStream)
             }
         }
-        // on below line we are adding catch block to handle exception
         catch (e: Exception) {
-            // on below line we are simply printing
-            // our exception and returning null
             e.printStackTrace()
             return null;
         }
-        // on below line we are returning input stream.
         return inputStream;
     }
     override fun onPostExecute(result: InputStream?) {
-        // on below line we are loading url within our
-        // pdf view on below line using input stream.
         mypdfView.fromStream(result).load()
     }
 
